0bc4e15684e5c54d070287b78e9b471ef2622280
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-03-27 20:09:32 +0000

    Create Input section

---
+++ src/components/InputSection.tsx
@@ -0,0 +1,9 @@
+export const InputSection = () => {
+  return (<>
+    <section className="my-2 flex">
+      <span>+</span>
+      <input className="w-10/12 mx-3" type="text" />
+      <span>Microphone</span>
+    </section>
+  </>)
+};
