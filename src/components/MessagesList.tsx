15159777679fc60c5095daac2618cdbd98ff5c79
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-03-27 21:34:52 +0000

    Format to markdown

--- src/components/MessagesList.tsx
+++ src/components/MessagesList.tsx
@@ -1,18 +1,21 @@
 import { Message } from "types/chat/Message.type";
 import "./MessagesList.css";
+import { useMarkDown } from "hooks/useMarkdown";
 
 type MessagesListProps = {
   messages: Message[];
 };
 
 export const MessagesList = ({ messages }: MessagesListProps) => {
+  const formatToMarkDown = useMarkDown();
+
   return (
     <>
       <ul className="w-full">
         {messages.map((message) => (
           <li className={`flex w-full ${message.role === 'User' ? 'justify-end' : ''}`}>
             <div className={`w-max max-w-[35rem] text-white mb-2 ${message.role === 'User' ? 'user-message' : ''}`}>
-              {message.content}
+              {formatToMarkDown(message.content)}
             </div>
           </li>
         ))}
