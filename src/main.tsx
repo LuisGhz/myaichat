aeb5f39aae9915264bb9b8975ef2a5a62a54e7e5
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-03-27 18:50:50 +0000

    Implement routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { Welcome } from 'components/Welcome.tsx'
+import { CurrentChat } from 'components/CurrentChat.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />} >
+          <Route index element={<Welcome />}></Route>
+          <Route path='chat' element={<CurrentChat />}></Route>
+          <Route path='chat/:id' element={<CurrentChat />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
