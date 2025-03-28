d0a0044a6ab23f5ef0b82697193cfe6743da529e
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-03-28 12:30:08 -0600

    Add basic message

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,3 +1,11 @@
 export const Welcome = () => {
-  return (<>Welcome</>);
-}
+  return (
+    <div className="text-white text-center pt-10">
+      <h1 className="text-2xl">Welcome to My AI Chat</h1>
+      <p>It supports the following models</p>
+      <ul>
+        <li>GPT-4O-MINI</li>
+      </ul>
+    </div>
+  );
+};
