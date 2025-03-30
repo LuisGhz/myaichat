38650ccabb62478d7916e09cd7dfc54b7e4e5941
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-03-30 15:03:41 -0600

    Add responsive menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { Outlet } from "react-router"
-import { ChatsList } from "components/ChatsList"
+import { Outlet } from "react-router";
+import { ChatsList } from "components/ChatsList";
 
 function App() {
-
   return (
-    <div className="flex h-full">
-      <div className="w-2xs">
-        <ChatsList />
-      </div>
+    <div className="flex h-full relative">
+      <ChatsList />
       <main className="grow bg-cop-3">
         <Outlet />
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ChatsList.tsx
+++ src/components/ChatsList.tsx
@@ -1,43 +1,69 @@
+import { ArrowLeftCircleIcon } from "assets/icons/ArrowLeftCircleIcon";
+import { ArrowRightCircleIcon } from "assets/icons/ArrowRightCircleIcon";
 import { PencilSquareIcon } from "assets/icons/PencilSquareIcon";
 import { TrashIcon } from "assets/icons/TrashIcon";
 import { AppContext } from "context/AppContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router";
 
 export const ChatsList = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
   const { chats, deleteChatById } = useContext(AppContext);
 
   return (
     <>
-      <ul className="bg-cop-4 text-white h-full px-3 overflow-y-auto hide-scrollbar">
-        <li>
-          <Link to={"/"}>Bienvenido</Link>
-        </li>
-        <li className="bg-cop-1 hover:bg-cop-2 cursor-pointer text-white px-2 py-1 text-center w-10/12 mx-auto rounded-sm transition-colors duration-300 my-3">
-          <Link className="flex justify-center" to={"/chat"}>
-            <PencilSquareIcon className="size-4 text-white me-2 mt-1" />
-            <span>New conversation</span>
-          </Link>
-        </li>
-        {chats.length > 0 &&
-          chats.map((chat) => (
-            <li
-              className="my-3 hover:bg-cop-6 transition-colors duration-300 rounded-lg flex justify-between"
-              key={chat.id}
-            >
-              <Link
-                to={`/chat/${chat.id}`}
-                className="block whitespace-nowrap overflow-hidden text-ellipsis h-11 px-2 py-2"
+      <div className="absolute top-0 left-0 z-50 flex text-white gap-x-2">
+        <Link to={"/"}>MyAIChat</Link>
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <ArrowRightCircleIcon className="size-4" />
+        </button>
+        <Link className="flex justify-center" to={"/chat"}>
+          <PencilSquareIcon className="size-4 text-white me-2 mt-1" />
+        </Link>
+      </div>
+      <div
+        className={`${
+          isMenuOpen ? "w-64" : "w-0"
+        } transition-width duration-500 md:relative bg-cop-4 `}
+      >
+        <ul
+          className={`bg-cop-4 text-white h-full px-3 overflow-y-auto overflow-x-hidden hide-scrollbar fixed md:relative top-0 left-0 z-50 transition-all duration-500 w-64 ${
+            isMenuOpen ? "translate-x-0" : "-translate-x-full"
+          } shadow-lg md:shadow-none`}
+        >
+          <li className="flex justify-between items-center px-2 py-1">
+            <Link to={"/"}>MyAIChat</Link>
+            <ArrowLeftCircleIcon
+              className="size-4 cursor-pointer text-white hover:text-gray-300 transition-colors duration-300"
+              onClick={() => setIsMenuOpen(false)}
+            />
+          </li>
+          <li className="bg-cop-1 hover:bg-cop-2 cursor-pointer text-white px-2 py-1 text-center w-10/12 mx-auto rounded-sm transition-colors duration-300 my-3">
+            <Link className="flex justify-center" to={"/chat"}>
+              <PencilSquareIcon className="size-4 text-white me-2 mt-1" />
+              <span>New conversation</span>
+            </Link>
+          </li>
+          {chats.length > 0 &&
+            chats.map((chat) => (
+              <li
+                className="my-3 hover:bg-cop-6 transition-colors duration-300 rounded-lg flex justify-between"
+                key={chat.id}
               >
-                {chat.title}
-              </Link>
-              <TrashIcon
-                className="w-4 min-w-4 text-white cursor-pointer hover:text-gray-600 transition-colors duration-300"
-                onClick={() => deleteChatById(chat.id)}
-              />
-            </li>
-          ))}
-      </ul>
+                <Link
+                  to={`/chat/${chat.id}`}
+                  className="block whitespace-nowrap overflow-hidden text-ellipsis h-11 px-2 py-2"
+                >
+                  {chat.title}
+                </Link>
+                <TrashIcon
+                  className="w-4 min-w-4 text-white cursor-pointer hover:text-gray-600 transition-colors duration-300"
+                  onClick={() => deleteChatById(chat.id)}
+                />
+              </li>
+            ))}
+        </ul>
+      </div>
     </>
   );
 };
