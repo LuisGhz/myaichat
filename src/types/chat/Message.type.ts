db1959b286d7085fe9116e7813ee0ec4403bcfc5
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-07 15:21:50 -0600

    Add role type

--- src/types/chat/Message.type.ts
+++ src/types/chat/Message.type.ts
@@ -1,6 +1,9 @@
+type Role = "User" | "Assistant";
+
 export type Message = {
-  role: string;
+  role: Role;
   content: string;
   promptTokens?: number;
   completionTokens?: number;
+  image?: File | string;
 }
