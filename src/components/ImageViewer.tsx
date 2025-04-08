14e3128f12f56b9332be209d8568ae24a305778a
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-08 16:09:57 -0600

    Create ImageViewer

---
+++ src/components/ImageViewer.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+type Props = {
+  image: File | string;
+};
+
+export const ImageViewer = ({ image }: Props) => {
+  const [finalImage, setFinalImage] = useState<string>("");
+
+  useEffect(() => {
+    if (typeof image === "string") {
+      setFinalImage(image);
+    } else {
+      const imageUrl = URL.createObjectURL(image);
+      setFinalImage(imageUrl);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <img
+        className="w-48 mb-3"
+        src={finalImage}
+        alt="Image uploaded by user"
+      />
+    </>
+  );
+};
