a526cd37cf86bb743559093ab648c0e641360113
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-13 09:11:34 -0600

    Only support jpg, jpeg, png and gif images

--- src/hooks/useAttachedFilesValidator.ts
+++ src/hooks/useAttachedFilesValidator.ts
@@ -3,7 +3,8 @@ export const useAttachedFilesValidator = () => {
   const MAX_FILE_SIZE = MB * 1024 * 1024;
 
   const validateFiles = (file: File) => {
-    return file.size <= MAX_FILE_SIZE && file.type.includes("image/");
+    const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
+    return file.size <= MAX_FILE_SIZE && allowedTypes.includes(file.type);
   };
 
   return { validateFiles };
