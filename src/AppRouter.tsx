6f6fa014dc41e1e25d64395d2b73838712c29d78
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-13 08:38:40 -0600

    Set routes to its own file

---
+++ src/AppRouter.tsx
@@ -0,0 +1,18 @@
+import App from "App";
+import { CurrentChat } from "components/CurrentChat";
+import { Welcome } from "components/Welcome";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route index element={<Welcome />}></Route>
+          <Route path="chat" element={<CurrentChat />}></Route>
+          <Route path="chat/:id" element={<CurrentChat />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { Welcome } from "components/Welcome.tsx";
-import { CurrentChat } from "components/CurrentChat.tsx";
 import { AppContextProvider } from "context/AppContextProvider.tsx";
+import { AppRouter } from "AppRouter.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<App />}>
-            <Route index element={<Welcome />}></Route>
-            <Route path="chat" element={<CurrentChat />}></Route>
-            <Route path="chat/:id" element={<CurrentChat />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <AppRouter />
     </AppContextProvider>
   </StrictMode>
 );
