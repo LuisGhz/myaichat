d32308068bad2fe48d7a76c83cc8b77e394f3f33
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-14 13:18:32 -0600

    Install vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
+// import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from '@tailwindcss/vite'
@@ -6,6 +8,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.js',
+  },
   plugins: [
     react(), 
     tsConfigPaths(), 
