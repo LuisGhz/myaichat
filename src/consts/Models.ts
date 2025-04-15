b44fc2391be090bcdbe55f1e65e0ce02f66ee1d4
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-15 14:26:58 -0600

    Create models const

---
+++ src/consts/Models.ts
@@ -0,0 +1,15 @@
+type Models = {
+  name: string;
+  value: string;
+}
+
+export const MODELS: Models[] = [
+  {
+    name: "GPT 4o",
+    value: "gpt-4o",
+  },
+  {
+    name: "GPT 4o Mini",
+    value: "gpt-4o-mini",
+  },
+];
