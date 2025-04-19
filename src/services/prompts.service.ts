7671689d4ee6bb1083b5b53ede1c098d94ddee54
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-19 13:48:09 -0600

    Add get prompts

--- src/services/prompts.service.ts
+++ src/services/prompts.service.ts
@@ -1,6 +1,11 @@
 import { apiClient } from "api";
+import { GetAllPromptsRes } from "types/prompts/GetAllPromptsRes.type";
 import { NewPromptReq } from "types/prompts/NewPromptReq.type";
 import { NewPromptRes } from "types/prompts/NewPromptRes.type";
+
+export const getPromptsService = () => {
+  return apiClient.get<GetAllPromptsRes>("/custom-prompts/all");
+}
 
 export const createPromptService = (req: NewPromptReq) => {
   return apiClient.post<NewPromptRes, NewPromptReq>("/custom-prompts", req);
