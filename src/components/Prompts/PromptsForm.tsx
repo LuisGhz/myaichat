2031d98d29284572e10020e7dc3d83e45bfffac9
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-19 15:09:47 -0600

    If params.id. load data to edit prompt

--- src/components/Prompts/PromptsForm.tsx
+++ src/components/Prompts/PromptsForm.tsx
@@ -6,9 +6,12 @@ import { InputContent } from "./inputs/InputContent";
 import { MessagesForm } from "./inputs/MessagesForm";
 import { ParamsForm } from "./inputs/ParamsForm";
 import { usePromptForm } from "hooks/usePromptForm";
-import { Link } from "react-router";
+import { Link, useParams } from "react-router";
+import { usePrompts } from "hooks/usePrompts";
+import { useEffect } from "react";
 
 export const PromptsForm = () => {
+  const params = useParams<{ id?: string }>();
   const {
     register,
     handleSubmit,
@@ -18,43 +21,71 @@ export const PromptsForm = () => {
     resolver: zodResolver(promptSchema),
     defaultValues: { params: [], messages: [] },
   });
-  const { onPromptFormSubmit } = usePromptForm();
+  const { onPromptFormSubmit, onPromptUpdateFormSubmit } = usePromptForm();
+  const { getPromptById, loading } = usePrompts();
 
-  const onSubmit = (data: PromptForm) => {
-    const res = onPromptFormSubmit(data);
+  useEffect(() => {
+    const fetchPrompt = async () => {
+      if (params.id) {
+        const prompt = await getPromptById(params.id);
+        if (prompt) {
+          setValue("name", prompt.name);
+          setValue("content", prompt.content);
+          setValue("messages", prompt.messages || []);
+          setValue("params", prompt.params || []);
+        }
+      }
+    };
+    fetchPrompt();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params.id]);
+
+  const onSubmit = async (data: PromptForm) => {
+    if (params.id) {
+      await onPromptUpdateFormSubmit(params.id!, data);
+      return;
+    }
+    const res = await onPromptFormSubmit(data);
     if (res) {
       console.log("Prompt created successfully:", res);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex flex-col h-full items-center justify-center text-white">
-        <h1 className="text-2xl font-bold">Prompt</h1>
-        <p className="text-lg mt-4">Create/Edit a prompt here.</p>
-        <InputName register={register} errors={errors} />
-        <InputContent register={register} errors={errors} />
-        {/* Messages Section */}
-        <MessagesForm register={register} errors={errors} setValue={setValue} />
-        {/* Dynamic Params Section */}
-        <ParamsForm register={register} errors={errors} setValue={setValue} />
-        <section className="flex gap-5">
-          <button
-            className="mt-8 w-36 p-2 bg-blue-600 hover:bg-blue-700 rounded-md font-semibold"
-            type="button"
-          >
-            <Link to={"/prompts"} className="text-white">
-              Go back
-            </Link>
-          </button>
-          <button
-            className="mt-8 w-36 p-2 bg-blue-600 hover:bg-blue-700 rounded-md font-semibold"
-            type="submit"
-          >
-            Save Prompt
-          </button>
-        </section>
-      </div>
-    </form>
+    <>
+      {params.id && loading && <p>loading</p>}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col h-full items-center justify-center text-white">
+          <h1 className="text-2xl font-bold">Prompt</h1>
+          <p className="text-lg mt-4">Create/Edit a prompt here.</p>
+          <InputName register={register} errors={errors} />
+          <InputContent register={register} errors={errors} />
+          {/* Messages Section */}
+          <MessagesForm
+            register={register}
+            errors={errors}
+            setValue={setValue}
+          />
+          {/* Dynamic Params Section */}
+          <ParamsForm register={register} errors={errors} setValue={setValue} />
+          <section className="flex gap-5">
+            <button
+              className="mt-8 w-36 p-2 bg-blue-600 hover:bg-blue-700 rounded-md font-semibold"
+              type="button"
+            >
+              <Link to={"/prompts"} className="text-white">
+                Go back
+              </Link>
+            </button>
+            <button
+              className="mt-8 w-36 p-2 bg-blue-600 hover:bg-blue-700 rounded-md font-semibold"
+              type="submit"
+            >
+              Save Prompt
+            </button>
+          </section>
+        </div>
+      </form>
+    </>
   );
 };
