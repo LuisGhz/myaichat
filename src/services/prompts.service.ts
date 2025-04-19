e5f1a894dd3dcbb74198c59d3fd24b17fd5be709
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-19 13:34:40 -0600

    Implement create prompts

--- src/services/prompts.service.ts
+++ src/services/prompts.service.ts
@@ -1,6 +1,7 @@
 import { apiClient } from "api";
 import { NewPromptReq } from "types/prompts/NewPromptReq.type";
+import { NewPromptRes } from "types/prompts/NewPromptRes.type";
 
 export const createPromptService = (req: NewPromptReq) => {
-  return apiClient.post("/custom-prompts", req);
+  return apiClient.post<NewPromptRes, NewPromptReq>("/custom-prompts", req);
 }
