bc6ce9910d9264836fb6c95dceeee802c04edcce
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-19 11:45:32 -0600

    Create input content

---
+++ src/components/Prompts/inputs/InputContent.tsx
@@ -0,0 +1,24 @@
+import { UseFormRegister, FieldErrors } from "react-hook-form";
+import { PromptForm } from "../PromptSchema";
+
+type Props = {
+  register: UseFormRegister<PromptForm>;
+  errors: FieldErrors<PromptForm>;
+};
+
+export const InputContent = ({ register, errors }: Props) => {
+  return (
+    <>
+      <div className="mt-6 w-96">
+        <textarea
+          className="w-full h-48 p-2 bg-gray-800 text-white rounded-md"
+          placeholder="Type your prompt here..."
+          {...register("content")}
+        ></textarea>
+        {errors.content && (
+          <span className="text-red-600 text-sm">{errors.content.message}</span>
+        )}
+      </div>
+    </>
+  );
+};
