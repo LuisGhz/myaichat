3dd5858df6a4bbb6e8aec94b5b0fa1746a71269f
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-19 15:10:02 -0600

    Create on prompt update form submit

--- src/hooks/usePromptForm.ts
+++ src/hooks/usePromptForm.ts
@@ -1,7 +1,8 @@
 import { PromptForm } from "components/Prompts/PromptSchema";
-import { createPromptService } from "services/prompts.service";
+import { createPromptService, updatePromptService } from "services/prompts.service";
 import { NewPromptReq } from "types/prompts/NewPromptReq.type";
 import { useToast } from "./useToast";
+import { UpdatePromptReq } from "types/prompts/UpdatePromptReq.type";
 
 export const usePromptForm = () => {
   const { toastSuccess, toastError } = useToast();
@@ -28,5 +29,28 @@ export const usePromptForm = () => {
     }
   };
 
-  return { onPromptFormSubmit };
+  const onPromptUpdateFormSubmit = async (id: string, data: PromptForm) => {
+    const req: UpdatePromptReq = {
+      id,
+      name: data.name,
+      content: data.content,
+      messages: data.messages?.map((message) => ({
+        role: message.role,
+        content: message.content,
+      })),
+      params: data.params?.map((param) => ({
+        name: param.name,
+        defaultValue: param.defaultValue,
+      })),
+    };
+
+    try {
+      toastSuccess("Prompt updated successfully!");
+      return await updatePromptService(req);
+    } catch (error) {
+      toastError(`${error}`);
+    }
+  }
+
+  return { onPromptFormSubmit, onPromptUpdateFormSubmit };
 };
