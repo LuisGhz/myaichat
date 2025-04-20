42dca3a73a5447ba6ee97ea667a2af58f6a39a0e
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-20 14:58:18 -0600

    Minor style changes and implement Link

--- src/components/Prompts/Prompts.tsx
+++ src/components/Prompts/Prompts.tsx
@@ -12,9 +12,9 @@ export const Prompts = () => {
         <div className="flex flex-col gap-2">
           <h2 className="text-xl font-semibold">Available Prompts</h2>
           {/* List of available prompts */}
-          <ul className="list-disc pl-5">
+          <ul className="pl-5">
             {prompts.prompts.map((prompt) => (
-              <li key={prompt.id} className="text-gray-300 cursor-pointer">
+              <li key={prompt.id} className="text-blue-500 cursor-pointer">
                 <Link to={`/prompts/form/${prompt.id}`}>{prompt.name}</Link>
               </li>
             ))}
@@ -25,12 +25,12 @@ export const Prompts = () => {
         <p className="text-gray-500">No prompts available.</p>
       )}
       <div className="flex justify-center mt-4">
-        <a
-          href="/prompts/form"
+        <Link
+          to="/prompts/form"
           className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-200"
         >
           Create New Prompt
-        </a>
+        </Link>
       </div>
     </div>
   );
