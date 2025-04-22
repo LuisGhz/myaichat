2a8e0812751bf3baffc9e0a426f1908650e15559
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-04-22 07:56:55 -0600

    Show available models from MODELS const

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,10 +1,18 @@
+import { MODELS } from "consts/Models";
+
 export const Welcome = () => {
   return (
     <div className="text-white text-center pt-10">
       <h1 className="text-2xl">Welcome to My AI Chat</h1>
       <p>It supports the following models</p>
       <ul>
-        <li>GPT-4O-MINI</li>
+        {
+          MODELS.map((model) => (
+            <li key={model.value} className="text-lg">
+              {model.name}
+            </li>
+          ))
+        }
       </ul>
     </div>
   );
