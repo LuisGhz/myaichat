89bd05689e3f9b98b22507d73548dfd4e4a96dbd
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-02 14:57:54 -0600

    Create context menu

---
+++ src/hooks/useContextMenu.ts
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const useChatsNavContextMenu = () => {
+  const onTouchStart = (
+    e: React.TouchEvent<HTMLElement>,
+    callback: () => void
+  ) => {
+    const timeout = setTimeout(() => {
+      callback();
+    }, 500); // 500ms for long press
+    (e.target as HTMLElement).dataset.longPressTimeout = String(timeout);
+  };
+
+  const onTouchEnd = (e: React.TouchEvent<HTMLElement>) => {
+    clearTimeout(Number((e.target as HTMLElement).dataset.longPressTimeout));
+  };
+
+  return {
+    onTouchStart,
+    onTouchEnd,
+  };
+};
