9dda0b21f761beb91180ffd043425b5a74928243
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-02 12:17:23 -0600

    Add InfoDialog component

--- src/components/InputSection/AttachFile.tsx
+++ src/components/InputSection/AttachFile.tsx
@@ -1,6 +1,7 @@
 import { PlusIcon } from "assets/icons/PlusIcon";
+import { InfoDialog } from "components/Dialogs/InfoDialog";
 import { useAttachedFilesValidator } from "hooks/useAttachedFilesValidator";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router";
 
 type Props = {
@@ -8,6 +9,7 @@ type Props = {
 };
 
 export const AttachFile = ({ onSelectImage }: Props) => {
+  const [isInfoDialogOpen, setInfoDialogOpen] = useState(false);
   const optionsRef = useRef<HTMLUListElement>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
   const inputFileRef = useRef<HTMLInputElement>(null);
@@ -40,6 +42,7 @@ export const AttachFile = ({ onSelectImage }: Props) => {
     if (!isValid) {
       console.error("Invalid file type or size exceeded.");
       event.target.value = "";
+      setInfoDialogOpen(true);
       return;
     }
     onSelectImage(file);
@@ -47,46 +50,56 @@ export const AttachFile = ({ onSelectImage }: Props) => {
   };
 
   return (
-    <button
-      className={`text-white cursor-pointer mb-1 transition-all duration-200 delay-150 p-1 rounded-full hover:bg-cop-6 relative`}
-      type="button"
-      aria-label="Attach file"
-      onClick={() => {
-        optionsRef.current?.classList.toggle("hidden");
-      }}
-      ref={buttonRef}
-    >
-      <PlusIcon className="size-5" />
-      <ul
-        className="hidden bg-cop-4 text-white rounded-lg mt-2 w-40 shadow-lg transition-all duration-200 delay-150 absolute z-10 bottom-full"
-        ref={optionsRef}
+    <>
+      <InfoDialog
+        message={[
+          "File type not supported (Only: jpg, jpeg, png, gif)",
+          "or file size exceeded (max 2 MB).",
+        ]}
+        isOpen={isInfoDialogOpen}
+        onConfirm={() => setInfoDialogOpen(false)}
+      />
+      <button
+        className={`text-white cursor-pointer mb-1 transition-all duration-200 delay-150 p-1 rounded-full hover:bg-cop-6 relative`}
+        type="button"
+        aria-label="Attach file"
+        onClick={() => {
+          optionsRef.current?.classList.toggle("hidden");
+        }}
+        ref={buttonRef}
       >
-        <li className="hover:bg-cop-6 rounded-t-lg transition-colors duration-200">
-          <Link
-            className="block py-1.5"
-            to="/chat"
-            onClick={() => console.log("asd")}
-          >
-            <span>New conversation</span>
-          </Link>
-        </li>
-        <li
-          className="py-1.5 hover:bg-cop-6 rounded-b-lg transition-colors duration-200"
-          onClick={makeClickToInputFile}
+        <PlusIcon className="size-5" />
+        <ul
+          className="hidden bg-cop-4 text-white rounded-lg mt-2 w-40 shadow-lg transition-all duration-200 delay-150 absolute z-10 bottom-full"
+          ref={optionsRef}
         >
-          Upload
-          <input
-            className="hidden"
-            accept="image/*"
-            type="file"
-            name="attach-file"
-            id="attach-file"
-            ref={inputFileRef}
-            onChange={onSelectFile}
-            data-testid="file-input"
-          />
-        </li>
-      </ul>
-    </button>
+          <li className="hover:bg-cop-6 rounded-t-lg transition-colors duration-200">
+            <Link
+              className="block py-1.5"
+              to="/chat"
+              onClick={() => console.log("asd")}
+            >
+              <span>New conversation</span>
+            </Link>
+          </li>
+          <li
+            className="py-1.5 hover:bg-cop-6 rounded-b-lg transition-colors duration-200"
+            onClick={makeClickToInputFile}
+          >
+            Upload
+            <input
+              className="hidden"
+              accept="image/jpeg, image/png, image/jpg, image/gif"
+              type="file"
+              name="attach-file"
+              id="attach-file"
+              ref={inputFileRef}
+              onChange={onSelectFile}
+              data-testid="file-input"
+            />
+          </li>
+        </ul>
+      </button>
+    </>
   );
 };
