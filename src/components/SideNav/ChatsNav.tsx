7ebcbb47b2ea2be74aaea939c349409c68bd0590
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-02 14:58:07 -0600

    Implement context menu component

--- src/components/SideNav/ChatsNav.tsx
+++ src/components/SideNav/ChatsNav.tsx
@@ -1,23 +1,21 @@
 import { ScreensWidth } from "consts/ScreensWidth";
 import { AppContext } from "context/AppContext";
-import { useContext } from "react";
+import { ReactNode, useContext, useRef, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
-import { ChatsContextMenu } from "./ChatsContextMenu";
-import { useChatsNavContextMenu } from "hooks/Components/SideNav/useChatsNavContextMenu";
+import { useChatsNavContextMenu } from "hooks/useContextMenu";
+import { ContextMenu } from "components/ContextMenu";
+import { TrashIcon } from "assets/icons/TrashIcon";
+import { PencilIcon } from "assets/icons/PencilIcon";
+import { ChatSummary } from "types/chat/ChatSummary.type";
 
 export const ChatsNav = () => {
   const { chats, deleteChatById, setIsMenuOpen } = useContext(AppContext);
   const navigate = useNavigate();
   const params = useParams();
-  const {
-    contextMenuLeft,
-    contextMenuTop,
-    currentContextMenu,
-    onContextMenu,
-    onTouchStart,
-    onTouchEnd,
-    closeContextMenu,
-  } = useChatsNavContextMenu();
+  const { onTouchStart, onTouchEnd } = useChatsNavContextMenu();
+  const [elements, setElements] = useState<ReactNode[]>([]);
+  const [isContextMenuOpen, setIsContextMenuOpen] = useState(false);
+  const triggeredContextMenu = useRef<HTMLElement>(null);
 
   const handleDeleteChat = (id: string) => {
     deleteChatById(id);
@@ -27,12 +25,57 @@ export const ChatsNav = () => {
   const handleRenameChat = (id: string) => {
     console.log("Rename chat", id);
     alert("Not available yet.");
-    closeContextMenu();
-  }
+    setIsContextMenuOpen(false);
+  };
 
   const handleRedirectToChatOnMobile = () => {
     if (window.innerWidth < ScreensWidth.tablet) setIsMenuOpen((prev) => !prev);
   };
+
+  const updateElements = (chat: ChatSummary) => {
+    setElements([
+      <button
+        className="cursor-pointer w-full text-red-700 flex gap-2"
+        type="button"
+        aria-label={`Delete chat: ${chat.title}`}
+        onClick={() => {
+          handleDeleteChat(chat.id);
+          setIsContextMenuOpen(false);
+        }}
+        onContextMenu={(e) => e.preventDefault()}
+      >
+        <TrashIcon className="w-5 min-w-5" />
+        <span>Delete</span>
+      </button>,
+      <button
+        className="cursor-pointer w-full text-white flex gap-2"
+        type="button"
+        aria-label="Rename chat"
+        onClick={() => handleRenameChat(chat.id)}
+      >
+        <PencilIcon className="w-5 min-w-5" />
+        <span>Rename</span>
+      </button>,
+    ]);
+  };
+
+  const handleContextMenu =
+    (chat: ChatSummary) => (e: React.MouseEvent<HTMLElement>) => {
+      e.preventDefault();
+      triggeredContextMenu.current = e.currentTarget;
+      updateElements(chat);
+      setIsContextMenuOpen(true);
+    };
+
+  const handleContextMenuOnTouch =
+    (chat: ChatSummary) => (e: React.TouchEvent<HTMLElement>) => {
+      onTouchStart(e, () => {
+        e.preventDefault();
+        triggeredContextMenu.current = e.currentTarget;
+        updateElements(chat);
+        setIsContextMenuOpen(true);
+      });
+    };
 
   return (
     <>
@@ -51,23 +94,21 @@ export const ChatsNav = () => {
                 }`}
                 aria-label={`Open chat: ${chat.title}`}
                 onClick={handleRedirectToChatOnMobile}
-                onTouchStart={(e) => onTouchStart(e, chat.id)}
-                onTouchEnd={(e) => onTouchEnd(e)}
-                onContextMenu={(e) => onContextMenu(e, chat.id)}
+                onContextMenu={handleContextMenu(chat)}
+                onTouchStart={handleContextMenuOnTouch(chat)}
+                onTouchEnd={onTouchEnd}
               >
                 {chat.title}
               </Link>
-              <ChatsContextMenu
-                chat={chat}
-                currentContextMenu={currentContextMenu}
-                top={contextMenuTop}
-                left={contextMenuLeft}
-                handleDeleteChat={handleDeleteChat}
-                handleRenameChat={handleRenameChat}
-              />
             </li>
           ))}
       </ul>
+      <ContextMenu
+        isOpen={isContextMenuOpen}
+        setIsOpen={setIsContextMenuOpen}
+        elements={elements}
+        triggered={triggeredContextMenu.current!}
+      />
     </>
   );
 };
