b1bc99ad8efe167156fef95c5baa5775841235f4
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-05 17:12:47 -0600

    Add gemini models

--- src/consts/Models.ts
+++ src/consts/Models.ts
@@ -9,15 +9,23 @@ export const MODELS: Models[] = [
     value: "gpt-4o",
   },
   {
-    name: "Gemini Pro",
-    value: "gemini-pro",
-  },
-  {
     name: "GPT 4o Mini",
     value: "gpt-4o-mini",
   },
   {
+    name: "Gemini 2.0 Flash Lite",
+    value: "gemini-2.0-flash-lite",
+  },
+  {
     name: "Gemini 2.0 Flash",
     value: "gemini-2.0-flash",
+  },
+  {
+    name: "Gemini 2.5 Flash Preview 04 17",
+    value: "gemini-2.5-flash-preview-04-17",
+  },
+  {
+    name: "Gemini 2.5 Pro preview 03 25",
+    value: "gemini-2.5-pro-preview-03-25",
   },
 ];
--- src/types/chat/Models.type.ts
+++ src/types/chat/Models.type.ts
@@ -1,1 +1,8 @@
-export type Models = "gpt-4o-mini" | "gpt-4o";
+export type Models =
+  | "gpt-4o-mini"
+  | "gpt-4o"
+  | "gemini-2.0-flash-lite"
+  | "gemini-2.0-flash"
+  | "gemini-2.5-flash-preview-04-17"
+  | "gemini-2.5-pro-preview-03-25";
+
