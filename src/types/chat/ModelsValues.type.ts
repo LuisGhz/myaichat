4b1563c37fead0963f9fc930d98749bb074b13bb
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-05 19:15:12 -0600

    Rename to model values

--- src/types/chat/ModelsValues.type.ts
+++ src/types/chat/ModelsValues.type.ts
@@ -1,8 +1,7 @@
-export type Models =
-  | "gpt-4o-mini"
+export type ModelsValues =
   | "gpt-4o"
+  | "gpt-4o-mini"
   | "gemini-2.0-flash-lite"
   | "gemini-2.0-flash"
   | "gemini-2.5-flash-preview-04-17"
   | "gemini-2.5-pro-preview-03-25";
-
