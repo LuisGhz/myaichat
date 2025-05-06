8cc2ae6595cd874f4054fca0296dd537ebeedb7d
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-05 20:49:28 -0600

    Show models data

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,18 +1,33 @@
 import { MODELS } from "consts/Models";
+import "./Welcome.css";
 
 export const Welcome = () => {
   return (
-    <div className="text-white text-center pt-10">
+    <div className="text-white text-center flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl">Welcome to My AI Chat</h1>
       <p>It supports the following models</p>
       <ul>
-        {
-          MODELS.map((model) => (
-            <li key={model.value} className="text-lg">
+        {MODELS.map((model) => (
+          <li className="link-container text-lg" key={model.value}>
+            <a
+              className="text-blue-500 underline cursor-pointer relative"
+              href={model.link}
+              target="_blank"
+            >
               {model.name}
-            </li>
-          ))
-        }
+            </a>
+            <section className="metadata bottom-full left-0">
+              <p className="font-bold">{model.name}</p>
+              <p>Context Window: {model.metadata.contextWindow}</p>
+              <p>Max Output Tokens: {model.metadata.maxOutputTokens}</p>
+              <p>Knowledge Cutoff: {model.metadata.knowledgeCutoff}</p>
+              <p>
+                Price: Input: ${model.price.input} / Output: $
+                {model.price.output}
+              </p>
+            </section>
+          </li>
+        ))}
       </ul>
     </div>
   );
