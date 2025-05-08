183c45b78bce7e8537d8fb9a29ad83e025bf161a
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-08 12:42:16 -0600

    Show cost by chat

--- src/components/CurrentModelSummary.tsx
+++ src/components/CurrentModelSummary.tsx
@@ -11,20 +11,23 @@ export const CurrentModelSummary = ({
   totalPromptTokens,
   totalCompletionTokens,
 }: Props) => {
-
-  const model = MODELS.find((model) => model.value === currentModel)?.name || currentModel;
+  const MILLION_TOKENS = 1000_000;
+  const { shortName, price } = MODELS.find(
+    (model) => model.value === currentModel
+  )!;
 
   return (
     <>
-      <div className="text-gray-700 text-xs flex justify-end gap-1.5 mb-1">
-        <span>Model: {model} -</span>
+      <div className="text-gray-600 text-xs flex justify-end gap-1.5">
+        <span>{shortName} -</span>
         <span>
-          PT:
-          {totalPromptTokens} -{" "}
+          PT:{totalPromptTokens} - $
+          {(((totalPromptTokens || 0) / MILLION_TOKENS) * price.input).toFixed(2)}
         </span>
         <span>
           CT:
-          {totalCompletionTokens}
+          {totalCompletionTokens} - $
+          {(((totalCompletionTokens || 0) / MILLION_TOKENS) * price.input).toFixed(2)}
         </span>
       </div>
     </>
