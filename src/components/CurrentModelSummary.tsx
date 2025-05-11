4150fcfe0b5b5355959efa814bfa6050eec81522
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-11 11:28:01 -0600

    Format tokens info

--- src/components/CurrentModelSummary.tsx
+++ src/components/CurrentModelSummary.tsx
@@ -1,4 +1,5 @@
 import { MODELS } from "consts/Models";
+import { useFormat } from "hooks/useFormat";
 
 type Props = {
   currentModel: string;
@@ -11,6 +12,7 @@ export const CurrentModelSummary = ({
   totalPromptTokens,
   totalCompletionTokens,
 }: Props) => {
+  const { fNumber } = useFormat();
   const MILLION_TOKENS = 1000_000;
   const { shortName, price } = MODELS.find(
     (model) => model.value === currentModel
@@ -21,12 +23,12 @@ export const CurrentModelSummary = ({
       <div className="text-gray-600 text-xs flex justify-end gap-1.5">
         <span>{shortName} -</span>
         <span>
-          PT:{totalPromptTokens} - $
+          PT:{fNumber(totalPromptTokens || 0)} - $
           {(((totalPromptTokens || 0) / MILLION_TOKENS) * price.input).toFixed(2)}
         </span>
         <span>
           CT:
-          {totalCompletionTokens} - $
+          {fNumber(totalCompletionTokens || 0)} - $
           {(((totalCompletionTokens || 0) / MILLION_TOKENS) * price.input).toFixed(2)}
         </span>
       </div>
