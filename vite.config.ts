9d76a44692a83d9a11f3c92b388cda110eced64b
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-05-15 11:16:49 -0600

    Add tests exclutions

--- vite.config.ts
+++ vite.config.ts
@@ -3,55 +3,65 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import tsConfigPaths from "vite-tsconfig-paths";
-import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './setupTests.js',
-    include: ['src/**/*.test.{ts,tsx}'],
+    environment: "jsdom",
+    setupFiles: "./setupTests.js",
+    include: ["src/**/*.test.{ts,tsx}"],
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      reportsDirectory: './coverage',
-      include: ['src/**/*.{ts,tsx}'],
-    }
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      reportsDirectory: "./coverage",
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: [
+        "src/main.tsx",
+        "src/AppRouter.tsx",
+        "src/hooks/useToast.ts",
+        "src/vite-env.d.ts",
+        "src/types/**",
+        "src/assets/**",
+        "src/consts/**",
+        "src/components/ChatsLoading.tsx"
+      ],
+    },
   },
   plugins: [
-    react(), 
-    tsConfigPaths(), 
+    react(),
+    tsConfigPaths(),
     tailwindcss(),
-    VitePWA({ 
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       manifest: {
-        name: 'My AI Chat',
-        short_name: 'MyAIChat',
-        description: 'An AI chat application',
-        theme_color: '#000000',
+        name: "My AI Chat",
+        short_name: "MyAIChat",
+        description: "An AI chat application",
+        theme_color: "#000000",
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
-        ]
-      }
-    })
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
   ],
   server: {
     host: true,
