7ea5411ca0abf0d1796eb1b66ed70a07c2b02204
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-06-03 13:30:11 -0600

    Create useAppStore to manage app state

---
+++ src/store/useAppStore.ts
@@ -0,0 +1,9 @@
+import { useZustandAppStore } from "./useZustandAppStore";
+
+export const useAppChatsStore = () => useZustandAppStore((state) => state.chats);
+export const useAppIsMenuOpenStore = () => useZustandAppStore((state) => state.isMenuOpen);
+export const useAppIsOfflineStore = () => useZustandAppStore((state) => state.isOffline);
+export const useAppSetChatsStore = () => useZustandAppStore((state) => state.setChats);
+export const useAppDeleteChatByIdStore = () => useZustandAppStore((state) => state.deleteChatById);
+export const useAppAddChatStore = () => useZustandAppStore((state) => state.addChat);
+export const useAppSetIsMenuOpenStore = () => useZustandAppStore((state) => state.setIsMenuOpen);
