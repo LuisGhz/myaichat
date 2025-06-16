c605743f6b4debab3b18c78a7305f1743d738bb6
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-06-16 13:27:34 -0600

    Add update chat title

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -1,10 +1,20 @@
 import { useZustandAppStore } from "./useZustandAppStore";
 
-export const useAppChatsStore = () => useZustandAppStore((state) => state.chats);
-export const useAppIsMenuOpenStore = () => useZustandAppStore((state) => state.isMenuOpen);
-export const useAppIsOfflineStore = () => useZustandAppStore((state) => state.isOffline);
-export const useAppSetChatsStore = () => useZustandAppStore((state) => state.setChats);
-export const useAppDeleteChatByIdStore = () => useZustandAppStore((state) => state.deleteChatById);
-export const useAppAddChatStore = () => useZustandAppStore((state) => state.addChat);
-export const useAppSetIsMenuOpenStore = () => useZustandAppStore((state) => state.setIsMenuOpen);
-export const useAppUpdateIsOfflineStore = () => useZustandAppStore((state) => state.updateIsOffline);
+export const useAppChatsStore = () =>
+  useZustandAppStore((state) => state.chats);
+export const useAppUpdateChatTitleStore = () =>
+  useZustandAppStore((state) => state.updateChatTitle);
+export const useAppIsMenuOpenStore = () =>
+  useZustandAppStore((state) => state.isMenuOpen);
+export const useAppIsOfflineStore = () =>
+  useZustandAppStore((state) => state.isOffline);
+export const useAppSetChatsStore = () =>
+  useZustandAppStore((state) => state.setChats);
+export const useAppDeleteChatByIdStore = () =>
+  useZustandAppStore((state) => state.deleteChatById);
+export const useAppAddChatStore = () =>
+  useZustandAppStore((state) => state.addChat);
+export const useAppSetIsMenuOpenStore = () =>
+  useZustandAppStore((state) => state.setIsMenuOpen);
+export const useAppUpdateIsOfflineStore = () =>
+  useZustandAppStore((state) => state.updateIsOffline);
