106bbde8124d6bde3d5ad3f2ad89494f68e9f980
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-06-17 12:05:51 -0600

    Update layout

--- src/features/Chat/components/CurrentModelSummary.tsx
+++ src/features/Chat/components/CurrentModelSummary.tsx
@@ -1,37 +1,38 @@
 import { MODELS } from "consts/Models";
 import { useFormat } from "hooks/useFormat";
+import { useCurrentChatStoreGetCurrentModelData } from "store/features/chat/useCurrentChatStore";
 
-type Props = {
-  currentModel: string;
-  totalPromptTokens?: number;
-  totalCompletionTokens?: number;
-};
-
-export const CurrentModelSummary = ({
-  currentModel,
-  totalPromptTokens,
-  totalCompletionTokens,
-}: Props) => {
+export const CurrentModelSummary = () => {
+  const currentModel = useCurrentChatStoreGetCurrentModelData();
   const { fNumber } = useFormat();
+  if (!currentModel) return null;
   const MILLION_TOKENS = 1000_000;
   const { shortName, price } = MODELS.find(
-    (model) => model.value === currentModel
+    (m) => m.value === currentModel?.model
   )!;
 
   return (
-    <>
-      <div className="text-gray-600 text-xs flex justify-end gap-1.5 mt-0.5">
-        <span>{shortName} -</span>
+    <div className="text-gray-400 text-xs flex flex-col md:flex-row justify-end md:justify-center items-center md:items-end gap-1.5 mt-0.5">
+      <p>
+        {shortName} <span className="hidden md:inline">-</span>{" "}
+      </p>
+      <div className="flex gap-1">
         <span>
-          PT:{fNumber(totalPromptTokens || 0)} - $
-          {(((totalPromptTokens || 0) / MILLION_TOKENS) * price.input).toFixed(2)}
+          PT:{fNumber(currentModel?.totalPromptTokens || 0)} - $
+          {(
+            ((currentModel?.totalPromptTokens || 0) / MILLION_TOKENS) *
+            price.input
+          ).toFixed(2)}
         </span>
         <span>
           CT:
-          {fNumber(totalCompletionTokens || 0)} - $
-          {(((totalCompletionTokens || 0) / MILLION_TOKENS) * price.output).toFixed(2)}
+          {fNumber(currentModel?.totalCompletionTokens || 0)} - $
+          {(
+            ((currentModel?.totalCompletionTokens || 0) / MILLION_TOKENS) *
+            price.output
+          ).toFixed(2)}
         </span>
       </div>
-    </>
+    </div>
   );
 };
