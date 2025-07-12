9542e32bbaca14fa8affe71ffa8bd088b0819e02
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-11 21:34:16 -0600

    Fix name typo

--- src/assets/icons/PaperClipIcon.tsx
+++ src/assets/icons/PaperClipIcon.tsx
@@ -2,7 +2,7 @@ import { SVGProps } from "react";
 
 type Props = SVGProps<SVGSVGElement>;
 
-export const PaperClip = (props: Props) => {
+export const PaperClipIcon = (props: Props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
