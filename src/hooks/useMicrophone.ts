8a1edc13e4110c803dcd06e5ed386237cc17f77f
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-15 15:58:11 -0600

    Use try catch and toast

--- src/hooks/useMicrophone.ts
+++ src/hooks/useMicrophone.ts
@@ -1,14 +1,22 @@
 import { transcribeAudioService } from "services/Microphone.service";
 import { useCurrentChatStoreSetIsSendingAudio } from "store/features/chat/useCurrentChatStore";
+import { useToast } from "./useToast";
 
 export const useMicrophone = () => {
   const setIsSendingAudio = useCurrentChatStoreSetIsSendingAudio();
+  const { toastError } = useToast();
 
   const transcribeAudio = async (audioBlob: Blob) => {
-    setIsSendingAudio(true);
-    const result = await transcribeAudioService(audioBlob);
-    setIsSendingAudio(false);
-    return result;
+    try {
+      setIsSendingAudio(true);
+      const result = await transcribeAudioService(audioBlob);
+      return result;
+    } catch (error) {
+      console.error("Error transcribing audio:", error);
+      toastError("Error transcribing audio. Please try again or try later.");
+    } finally {
+      setIsSendingAudio(false);
+    }
   };
 
   return {
