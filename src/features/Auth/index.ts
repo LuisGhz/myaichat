dac79da86831bc2f94837f0aa9245bd7b1739f87
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-25 18:10:10 -0600

    Remove simple profile

--- src/features/Auth/index.ts
+++ src/features/Auth/index.ts
@@ -3,4 +3,3 @@ export { OAuth2Callback } from './OAuth2Callback';
 export { SimpleOAuth2Login } from './SimpleOAuth2Login';
 export { SimpleOAuth2Provider } from './SimpleOAuth2Provider';
 export { SimpleProtectedRoute } from './SimpleProtectedRoute';
-export { SimpleUserProfile } from './SimpleUserProfile';
