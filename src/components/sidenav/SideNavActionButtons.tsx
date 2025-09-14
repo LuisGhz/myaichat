f494e6a2316d0a827999a94bfc94fc95da022d24
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-14 13:20:03 -0600

    Create SideNavActionButtons

---
+++ src/components/sidenav/SideNavActionButtons.tsx
@@ -0,0 +1,23 @@
+import { PencilSquareIcon } from "icons/PencilSquareIcon";
+import { Prompt16FilledIcon } from "./Prompt16FilledIcon";
+
+export const SideNavActionButtons = () => {
+  return (
+    <div className="flex flex-col gap-2 px-4">
+      <button
+        className="bg-blue-500 hover:bg-blue-400 dark:bg-gray-950 dark:hover:bg-gray-950 dark:hover:outline-1 dark:hover:outline-gray-500 text-white py-1.5 rounded flex items-center justify-center gap-2 cursor-pointer"
+        aria-label="New conversation"
+      >
+        <PencilSquareIcon className="inline w-4 h-4" />
+        <span className="text-sm">New conversation</span>
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-400 dark:bg-gray-950 dark:hover:bg-gray-950 dark:hover:outline-1 dark:hover:outline-gray-500 text-white py-1.5 rounded flex items-center justify-center gap-2 cursor-pointer"
+        aria-label="Prompts"
+      >
+        <Prompt16FilledIcon className="inline w-5 h-5" />
+        <span className="text-sm">Prompts</span>
+      </button>
+    </div>
+  );
+};
