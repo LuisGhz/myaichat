789e67c90a80c08a1049de390637d5ac83a2517e
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-14 10:30:40 -0600

    Install vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
+    test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.js",
+    include: ["src/**/*.test.{ts,tsx}"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      reportsDirectory: "./coverage",
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: [
+        "src/main.tsx",
+        "src/vite-env.d.ts",
+        "src/types/**",
+        "src/assets/**",
+        "src/consts/**",
+        "src/store/**",
+      ],
+    },
+  },
   plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
