60c88923e25b8715b74e87cac73ebd9e89353555
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-15 11:58:47 -0600

    Implement Antd theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AppRouter } from "AppRouter.tsx";
+import { AntdThemeProvider } from "theme/AntdThemeProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppRouter />
+    <AntdThemeProvider>
+      <AppRouter />
+    </AntdThemeProvider>
   </StrictMode>
 );
