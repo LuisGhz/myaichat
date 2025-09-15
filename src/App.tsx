133602394e4210f6cf3ab7dbf7a1c7cf2c9dfc3f
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-15 07:22:43 -0600

    Basic header and outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { Layout } from "antd";
-import { SideNav } from "components/SideNav";
+import { SideNav } from "components/sidenav/SideNav";
+import { Outlet } from "react-router";
+import { useAppStore, useAppStoreActions } from "store/app/AppStore";
 
-const { Content } = Layout;
+const { Content, Header } = Layout;
 
 function App() {
+  const { sideNavCollapsed } = useAppStore();
+  const { setSideNavCollapsed } = useAppStoreActions();
   return (
     <Layout className="h-dvh">
       <SideNav />
       <Layout>
+        <Header className="bg-white shadow-sm flex items-center px-4">
+          <h1 className="text-lg font-semibold">My AI Chat</h1>
+          <button onClick={() => setSideNavCollapsed(!sideNavCollapsed)}>
+            Toggle SideNav
+          </button>
+        </Header>
         <Content>
-          <p>Content</p>
+          <Outlet />
         </Content>
       </Layout>
     </Layout>
