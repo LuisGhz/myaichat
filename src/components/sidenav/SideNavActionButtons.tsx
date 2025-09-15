c0d3c74deb1fd6271da9940d259c13333685b270
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-15 07:27:23 -0600

    Set link to buttons

--- src/components/sidenav/SideNavActionButtons.tsx
+++ src/components/sidenav/SideNavActionButtons.tsx
@@ -1,12 +1,16 @@
+import { useNavigate } from "react-router";
 import { PencilSquareIcon } from "icons/PencilSquareIcon";
 import { Prompt16FilledIcon } from "./Prompt16FilledIcon";
 
 export const SideNavActionButtons = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col gap-2 px-4">
       <button
         className="bg-blue-500 hover:bg-blue-400 dark:bg-gray-950 dark:hover:bg-gray-950 dark:hover:outline-1 dark:hover:outline-gray-500 text-white py-1.5 rounded flex items-center justify-center gap-2 cursor-pointer"
         aria-label="New conversation"
+        onClick={() => navigate("/chat")}
       >
         <PencilSquareIcon className="inline w-4 h-4" />
         <span className="text-sm">New conversation</span>
@@ -14,6 +18,7 @@ export const SideNavActionButtons = () => {
       <button
         className="bg-blue-500 hover:bg-blue-400 dark:bg-gray-950 dark:hover:bg-gray-950 dark:hover:outline-1 dark:hover:outline-gray-500 text-white py-1.5 rounded flex items-center justify-center gap-2 cursor-pointer"
         aria-label="Prompts"
+        onClick={() => navigate("/prompts")}
       >
         <Prompt16FilledIcon className="inline w-5 h-5" />
         <span className="text-sm">Prompts</span>
