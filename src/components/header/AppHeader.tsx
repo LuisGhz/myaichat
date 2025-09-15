e76b7d71cffb35df6b732625b8997a1179af6115
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-15 10:39:33 -0600

    Modify toggle button

--- src/components/header/AppHeader.tsx
+++ src/components/header/AppHeader.tsx
@@ -1,13 +1,26 @@
 import { Layout } from "antd";
+import { MenuFoldRight } from "icons/MenuFoldRightIcon";
+import { useAppStore, useAppStoreActions } from "store/app/AppStore";
 
 export const AppHeader = () => {
+  const { sideNavCollapsed } = useAppStore();
+  const { setSideNavCollapsed } = useAppStoreActions();
   const { Header } = Layout;
   return (
     <Header
-      className="bg-white shadow-sm flex items-center px-4"
-      style={{ height: 32, backgroundColor: "white" }}
+      className="!bg-gray-100 dark:!bg-gray-900 shadow-sm flex items-center !px-1"
+      style={{ height: 32 }}
     >
-      <h1 className="text-lg font-semibold">My AI Chat</h1>
+      {sideNavCollapsed ? (
+        <button
+          className="me-2"
+          onClick={() => setSideNavCollapsed(!sideNavCollapsed)}
+          aria-label="Expand side navigation"
+        >
+          <MenuFoldRight className="w-5 h-5 text-gray-600 dark:text-gray-300 cursor-pointer" />
+        </button>
+      ) : null}
+      <h1 className="text-lg font-semibold dark:text-white">My AI Chat</h1>
     </Header>
   );
 };
