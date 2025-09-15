59726471fbf07e4abfa40855079df1ac3faeba10
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-15 07:35:45 -0600

    Add chat path

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,5 +1,6 @@
 import App from "App";
 import { Login } from "features/Auth/pages/Login";
+import { Chat } from "features/Chat/pages/Chat";
 import { Welcome } from "features/Welcome/Welcome";
 import { BrowserRouter, Route, Routes } from "react-router";
 
@@ -10,6 +11,8 @@ export const AppRouter = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<App />}>
           <Route index element={<Welcome />} />
+          <Route path="chat" element={<Chat />} />
+          <Route path="chat/:id" element={<Chat />} />
         </Route>
       </Routes>
     </BrowserRouter>
