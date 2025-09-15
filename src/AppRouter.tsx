03112248f28e3168368b014c0c18c69403e25549
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-15 15:07:05 -0600

    Add promtps path

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,8 @@
 import App from "App";
 import { Login } from "features/Auth/pages/Login";
 import { Chat } from "features/Chat/pages/Chat";
+import { CreateEditPrompt } from "features/Prompts/pages/CreateEditPromtp";
+import { Prompts } from "features/Prompts/pages/Prompts";
 import { Welcome } from "features/Welcome/Welcome";
 import { BrowserRouter, Route, Routes } from "react-router";
 
@@ -13,6 +15,9 @@ export const AppRouter = () => {
           <Route index element={<Welcome />} />
           <Route path="chat" element={<Chat />} />
           <Route path="chat/:id" element={<Chat />} />
+          <Route path="prompts" element={<Prompts />} />
+          <Route path="prompts/form" element={<CreateEditPrompt />} />
+          <Route path="prompts/form/:id" element={<CreateEditPrompt />} />
         </Route>
       </Routes>
     </BrowserRouter>
