493fdd7e0431b6c174dfa5c04b7d5a2bd11c8e27
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-16 20:38:32 -0600

    Create ModelValues

---
+++ src/types/chat/ModelsValues.d.ts
@@ -0,0 +1,10 @@
+type ModelsValues =
+  | "gpt-4o"
+  | "gpt-4o-mini"
+  | "gpt-4.1-2025-04-14"
+  | "gpt-4.1-mini-2025-04-14"
+  | "o4-mini"
+  | "gemini-2.0-flash-lite"
+  | "gemini-2.0-flash"
+  | "gemini-2.5-flash"
+  | "gemini-2.5-pro";
