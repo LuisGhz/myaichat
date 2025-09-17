bc767684190f414ea001253e760520e9b0eb6091
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-16 20:37:49 -0600

    Create CurrentChatMetadata

---
+++ src/features/Chat/components/InputSection/CurrentChatMetadata.tsx
@@ -0,0 +1,49 @@
+import { Popover } from "antd";
+import { ExclamationCircleIcon } from "icons/ExclamationCircleIcon";
+import { useChatStore } from "store/app/ChatStore";
+
+export const CurrentChatMetadata = () => {
+  const { currentChatMetadata } = useChatStore();
+
+  if (!currentChatMetadata) return null;
+
+  const {
+    totalPromptTokens,
+    totalCompletionTokens,
+    maxOutputTokens,
+    isWebSearchMode,
+    model,
+  } = currentChatMetadata;
+
+  const content = (
+    <ul className="flex flex-col gap-1">
+      <li>
+        <span className="font-semibold">Total Prompt Tokens</span>:{" "}
+        {totalPromptTokens}
+      </li>
+      <li>
+        <span className="font-semibold">Total Completion Tokens</span>:{" "}
+        {totalCompletionTokens}
+      </li>
+      <li>
+        <span className="font-semibold">Max Output Tokens</span>:{" "}
+        {maxOutputTokens}
+      </li>
+      <li>
+        <span className="font-semibold">Is Web Search Mode</span>:{" "}
+        {isWebSearchMode ? "Yes" : "No"}
+      </li>
+      <li>
+        <span className="font-semibold">Model</span>: {model}
+      </li>
+    </ul>
+  );
+
+  return (
+    <span>
+      <Popover content={content}>
+        <ExclamationCircleIcon className="w-6 h-6 cursor-pointer fill-gray-700 dark:fill-gray-200" />
+      </Popover>
+    </span>
+  );
+};
