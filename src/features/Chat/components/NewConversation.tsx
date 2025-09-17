a50df7ed733241715b25e3fa0c3c64c6c916645c
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-16 20:04:02 -0600

    Handle form values with Zustand store

--- src/features/Chat/components/NewConversation.tsx
+++ src/features/Chat/components/NewConversation.tsx
@@ -1,6 +1,19 @@
 import { Select } from "antd";
+import { MODELS } from "core/const/Models";
+import { usePrompts } from "features/Prompts/hooks/usePrompts";
+import { useEffect } from "react";
+import { useChatStore, useChatStoreActions } from "store/app/ChatStore";
 
 export const NewConversation = () => {
+  const { getAllPrompts, promptsSummary } = usePrompts();
+  const { model, promptId } = useChatStore();
+  const { setModel, setPromptId } = useChatStoreActions();
+
+  useEffect(() => {
+    getAllPrompts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center h-full">
       <h2 className="text-2xl font-bold mb-4 text-gray-700 dark:text-gray-200">
@@ -10,23 +23,25 @@ export const NewConversation = () => {
         <div>
           <Select
             className="w-56"
-            options={[
-              { value: "gem-2.0", label: "Gemini 2.0" },
-              { value: "gpt-4om", label: "GPT 4o mini" },
-              { value: "gpt-5", label: "GPT 5" },
-            ]}
-            value={"gem-2.0"}
+            options={MODELS.map((model) => ({
+              value: model.value,
+              label: model.name,
+            }))}
+            value={model}
+            onChange={(value) => setModel(value as ModelsValues)}
           />
         </div>
         <div>
           <Select
             className="w-56 dark:!bg-gray-800"
-            options={[
-              { value: "oj12o3ij1oi2ejd", label: "Tech interview" },
-              { value: "asdasd1221d", label: "Barra coba" },
-              { value: "asda1wqdd12d", label: "English teacher" },
-            ]}
+            options={promptsSummary.map((prompt) => ({
+              value: prompt.id,
+              label: prompt.name,
+            }))}
             placeholder="Select a prompt if you wish"
+            value={promptId}
+            onChange={(value) => setPromptId(value as string)}
+            allowClear
           />
         </div>
       </section>
