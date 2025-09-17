498a3d8cbb0b8387671a62fe283855e9a66c19bf
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-17 10:59:25 -0600

    Add setIsWebSearchMode

--- src/store/app/ChatStore.ts
+++ src/store/app/ChatStore.ts
@@ -3,18 +3,29 @@ import { useChatZustandStore } from "store/ChatZustandStore";
 export const useChatStore = () => {
   const model = useChatZustandStore((state) => state.model);
   const maxOutputTokens = useChatZustandStore((state) => state.maxOutputTokens);
+  const isWebSearchMode = useChatZustandStore((state) => state.isWebSearchMode);
   const promptId = useChatZustandStore((state) => state.promptId);
   const currentChatMetadata = useChatZustandStore(
     (state) => state.currentChatMetadata
   );
   const messages = useChatZustandStore((state) => state.messages);
-  return { model, maxOutputTokens, promptId, currentChatMetadata, messages };
+  return {
+    model,
+    maxOutputTokens,
+    isWebSearchMode,
+    promptId,
+    currentChatMetadata,
+    messages,
+  };
 };
 
 export const useChatStoreActions = () => {
   const setModel = useChatZustandStore((state) => state.setModel);
   const setMaxOutputTokens = useChatZustandStore(
     (state) => state.setMaxOutputTokens
+  );
+  const setIsWebSearchMode = useChatZustandStore(
+    (state) => state.setIsWebSearchMode
   );
   const setPromptId = useChatZustandStore((state) => state.setPromptId);
   const setCurrentChatMetadata = useChatZustandStore(
@@ -27,5 +38,6 @@ export const useChatStoreActions = () => {
     setPromptId,
     setCurrentChatMetadata,
     setMessages,
+    setIsWebSearchMode,
   };
 };
