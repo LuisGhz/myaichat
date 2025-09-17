ad8a88d4cbec479a56a469704b919688229cde43
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-16 20:24:00 -0600

    Import chat store

--- src/features/Chat/pages/Chat.tsx
+++ src/features/Chat/pages/Chat.tsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
-import { useParams } from "react-router";
+import { useEffect } from "react";
 import { NewConversation } from "../components/NewConversation";
 import { InputSection } from "../components/InputSection/InputSection";
+import { useChatParams } from "../hooks/useChatParams";
+import { useChat } from "../hooks/useChat";
+import { ChatMessages } from "../components/ChatMessages";
 
 export const Chat = () => {
-  const params = useParams();
-  const [messages, setMessages] = useState<string[]>([]);
+  const params = useChatParams();
+  const { resetChatData, getChatMessages, messages } = useChat();
+
+  useEffect(() => {
+    if (!params.id) {
+      resetChatData();
+      return;
+    }
+
+    getChatMessages(params.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params.id]);
+
   return (
     <div className="h-full flex flex-col">
       <section className="grow">
         {messages.length === 0 && <NewConversation />}
+        {messages.length > 0 && <ChatMessages messages={messages} />}
       </section>
       <InputSection />
     </div>
