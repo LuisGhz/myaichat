ebce2ae38f8240ddd78ed9ad756703be38db7686
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-18 12:04:38 -0600

    Refacto styles classes

--- src/features/Chat/components/InputSection/InputActionButton/InputActionButtons.tsx
+++ src/features/Chat/components/InputSection/InputActionButton/InputActionButtons.tsx
@@ -6,18 +6,18 @@ import { MicrophoneButton } from "./MicrophoneButton";
 
 export const InputActionButtons = () => {
   const params = useChatParams();
+  const buttonsStyles =
+    "cursor-pointer rounded-full p-1.5 hover:bg-gray-300 dark:hover:bg-gray-700 transition-c-200";
 
   return (
     <section className="flex justify-between">
       <div className="flex gap-2">
-        <SettingsChatButton buttonClassName="cursor-pointer rounded-full p-1.5 hover:bg-gray-300 dark:hover:bg-gray-700 transition-c-200" />
-        {params.id && (
-          <CurrentChatMetadata buttonClassName="cursor-pointer rounded-full p-1.5 hover:bg-gray-300 dark:hover:bg-gray-700 transition-c-200" />
-        )}
+        <SettingsChatButton buttonClassName={buttonsStyles} />
+        {params.id && <CurrentChatMetadata buttonClassName={buttonsStyles} />}
       </div>
       <div className="flex gap-2">
-        <AttachFileButton buttonClassName="cursor-pointer rounded-full p-1.5 hover:bg-gray-300 dark:hover:bg-gray-700 transition-c-200" />
-        <MicrophoneButton buttonClassName="cursor-pointer rounded-full p-1.5 hover:bg-gray-300 dark:hover:bg-gray-700 transition-c-200" />
+        <AttachFileButton buttonClassName={buttonsStyles} />
+        <MicrophoneButton buttonClassName={buttonsStyles} />
       </div>
     </section>
   );
