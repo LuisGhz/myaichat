a1ff53d67a6f1ad0f730cdd231d5a905e86e24b7
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-18 16:18:04 -0600

    Upodate max tokens and web search toggle

--- src/features/Chat/components/InputSection/InputActionButton/SettingsChatButton.tsx
+++ src/features/Chat/components/InputSection/InputActionButton/SettingsChatButton.tsx
@@ -1,6 +1,9 @@
+import { useChat } from "features/Chat/hooks/useChat";
+import { useChatParams } from "features/Chat/hooks/useChatParams";
 import { ChatConfigModal } from "features/Chat/modals/ChatConfigModal";
 import { FileTypeLightConfigIcon } from "icons/FileTypeLightConfigIcon";
 import { useState } from "react";
+import { useChatStore, useChatStoreActions } from "store/app/ChatStore";
 
 type Props = {
   buttonClassName?: string;
@@ -8,6 +11,26 @@ type Props = {
 
 export const SettingsChatButton = ({ buttonClassName }: Props) => {
   const [isChatConfigModalOpen, setIsChatConfigModalOpen] = useState(false);
+  const { toggleIsWebSearchMode, changeMaxOutputTokens } = useChat();
+  const { maxOutputTokens, isWebSearchMode } = useChatStore();
+  const { setMaxOutputTokens, setIsWebSearchMode } = useChatStoreActions();
+  const params = useChatParams();
+
+  const openModal = () => {
+    setIsChatConfigModalOpen(true);
+  };
+
+  const handleNewConfig = (newConfig: ChatConfigOnClose) => {
+    if (maxOutputTokens !== newConfig.maxOutputTokens) {
+      setMaxOutputTokens(newConfig.maxOutputTokens);
+      if (params.id)
+        changeMaxOutputTokens(params.id, newConfig.maxOutputTokens);
+    }
+    if (isWebSearchMode !== newConfig.isWebSearchMode) {
+      setIsWebSearchMode(newConfig.isWebSearchMode);
+      if (params.id) toggleIsWebSearchMode(params.id);
+    }
+  };
 
   return (
     <>
@@ -16,18 +39,18 @@ export const SettingsChatButton = ({ buttonClassName }: Props) => {
         type="button"
         aria-label="Settings"
         title="Settings"
-        onClick={() => setIsChatConfigModalOpen(true)}
+        onClick={openModal}
       >
         <FileTypeLightConfigIcon className="w-6 h-6 cursor-pointer fill-gray-700 dark:fill-gray-200" />
       </button>
       {isChatConfigModalOpen && (
         <ChatConfigModal
           onClose={(newConfig) => {
-            console.log("New Chat Config:", newConfig);
+            handleNewConfig(newConfig);
             setIsChatConfigModalOpen(false);
           }}
-          currentMaxOutputTokens={1000}
-          currentIsWebSearchMode={false}
+          currentMaxOutputTokens={maxOutputTokens}
+          currentIsWebSearchMode={isWebSearchMode}
         />
       )}
     </>
