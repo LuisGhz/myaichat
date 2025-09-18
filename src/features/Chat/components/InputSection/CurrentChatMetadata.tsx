2a04d097cfb3a1c8cc2ae9455874c14f37f64e45
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-18 11:14:11 -0600

    Show price for current model

--- src/features/Chat/components/InputSection/CurrentChatMetadata.tsx
+++ src/features/Chat/components/InputSection/CurrentChatMetadata.tsx
@@ -1,9 +1,12 @@
 import { Popover } from "antd";
+import { useCurrentChatMetadata } from "features/Chat/hooks/useCurrentChatMetadata";
 import { ExclamationCircleIcon } from "icons/ExclamationCircleIcon";
 import { useChatStore } from "store/app/ChatStore";
 
 export const CurrentChatMetadata = () => {
   const { currentChatMetadata } = useChatStore();
+  const { formatCost, calculatePromptTokens, calculateCompletionTokens } =
+    useCurrentChatMetadata(currentChatMetadata?.model || "");
 
   if (!currentChatMetadata) return null;
 
@@ -19,6 +22,14 @@ export const CurrentChatMetadata = () => {
       <li>
         <span className="font-semibold">Total Completion Tokens</span>:{" "}
         {totalCompletionTokens}
+      </li>
+      <li>
+        <span className="font-semibold">Prompt cost (USD)</span>:{" "}
+        {formatCost(calculatePromptTokens(totalPromptTokens))}
+      </li>
+      <li>
+        <span className="font-semibold">Completion cost (USD)</span>:{" "}
+        {formatCost(calculateCompletionTokens(totalCompletionTokens))}
       </li>
       <li>
         <span className="font-semibold">Model</span>: {model}
