1dae0955a7ac4a27edcc52c91069f101ce9fdac3
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-18 10:55:44 -0600

    Show AssisantTyping while waiting for response

--- src/features/Chat/components/ChatMessages.tsx
+++ src/features/Chat/components/ChatMessages.tsx
@@ -1,3 +1,4 @@
+import { AssistantTyping } from "./AssistantTyping";
 import { MessageActionButtons } from "./MessageActionButtons";
 
 type Props = {
@@ -7,25 +8,36 @@ type Props = {
 export const ChatMessages = ({ messages }: Props) => {
   return (
     <section className="flex flex-col gap-10 px-1 md:px-2">
-      {messages.map((msg, idx) => (
-        <div
-          className={`${
-            msg.role === "User" ? "self-end" : "self-start"
-          } max-w-[70%] p-3 rounded-lg app-text bg-gray-300 dark:bg-gray-950 relative`}
-          key={idx}
-        >
-          <p className="text-[1rem]">{msg.content}</p>
-          {((msg.completionTokens || 0) > 0 || (msg.promptTokens || 0) > 0) && (
-            <>
-              <span className="text-xs block mt-1.5 app-text">
-                Tokens:{" "}
-                {msg.role === "User" ? msg.promptTokens : msg.completionTokens}
-              </span>
-              <MessageActionButtons role={msg.role} content={msg.content} />
-            </>
-          )}
-        </div>
-      ))}
+      {messages.map((msg, idx) =>
+        msg.content.trim() === "" ? (
+          msg.role === "Assistant" ? (
+            <AssistantTyping key={idx} />
+          ) : (
+            <></>
+          )
+        ) : (
+          <div
+            className={`${
+              msg.role === "User" ? "self-end" : "self-start"
+            } max-w-[70%] p-3 rounded-lg app-text bg-gray-300 dark:bg-gray-950 relative`}
+            key={idx}
+          >
+            <p className="text-[1rem]">{msg.content}</p>
+            {((msg.completionTokens || 0) > 0 ||
+              (msg.promptTokens || 0) > 0) && (
+              <>
+                <span className="text-xs block mt-1.5 app-text">
+                  Tokens:{" "}
+                  {msg.role === "User"
+                    ? msg.promptTokens
+                    : msg.completionTokens}
+                </span>
+                <MessageActionButtons role={msg.role} content={msg.content} />
+              </>
+            )}
+          </div>
+        )
+      )}
     </section>
   );
 };
