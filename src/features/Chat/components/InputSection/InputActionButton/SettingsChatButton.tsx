d0a197cb0b0fa9a5ee462e6c65d3e273da3b580b
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-18 14:50:43 -0600

    Add ChatConfigModal

--- src/features/Chat/components/InputSection/InputActionButton/SettingsChatButton.tsx
+++ src/features/Chat/components/InputSection/InputActionButton/SettingsChatButton.tsx
@@ -1,18 +1,35 @@
+import { ChatConfigModal } from "features/Chat/modals/ChatConfigModal";
 import { FileTypeLightConfigIcon } from "icons/FileTypeLightConfigIcon";
+import { useState } from "react";
 
 type Props = {
   buttonClassName?: string;
 };
 
 export const SettingsChatButton = ({ buttonClassName }: Props) => {
+  const [isChatConfigModalOpen, setIsChatConfigModalOpen] = useState(false);
+
   return (
-    <button
-      className={`${buttonClassName}`}
-      type="button"
-      aria-label="Settings"
-      title="Settings"
-    >
-      <FileTypeLightConfigIcon className="w-6 h-6 cursor-pointer fill-gray-700 dark:fill-gray-200" />
-    </button>
+    <>
+      <button
+        className={`${buttonClassName}`}
+        type="button"
+        aria-label="Settings"
+        title="Settings"
+        onClick={() => setIsChatConfigModalOpen(true)}
+      >
+        <FileTypeLightConfigIcon className="w-6 h-6 cursor-pointer fill-gray-700 dark:fill-gray-200" />
+      </button>
+      {isChatConfigModalOpen && (
+        <ChatConfigModal
+          onClose={(newConfig) => {
+            console.log("New Chat Config:", newConfig);
+            setIsChatConfigModalOpen(false);
+          }}
+          currentMaxOutputTokens={1000}
+          currentIsWebSearchMode={false}
+        />
+      )}
+    </>
   );
 };
