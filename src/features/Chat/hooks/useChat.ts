3ecdaee90f20856001db166ba53384f8b7dc2bce
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-17 20:08:43 -0600

    Add setIsGettingChat

--- src/features/Chat/hooks/useChat.ts
+++ src/features/Chat/hooks/useChat.ts
@@ -11,7 +11,7 @@ export const useChat = () => {
   const { setModel, setPromptId, setCurrentChatMetadata, setMessages } =
     useChatStoreActions();
   const { chatsSummary } = useAppStore();
-  const { setChatsSummary } = useAppStoreActions();
+  const { setChatsSummary, setIsGettingNewChat } = useAppStoreActions();
 
   const resetChatData = () => {
     setModel(DEFAULT_MODEL);
@@ -52,6 +52,7 @@ export const useChat = () => {
     };
     const mergedMessages = [...messages, newMessage];
     setMessages(mergedMessages);
+    setIsGettingNewChat(true);
     try {
       const res = await sendNewMessageService(formData);
       if (res?.isNew) {
