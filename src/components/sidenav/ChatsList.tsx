5d930e95629f130278f45aef8e0bbe6abd784a53
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-18 09:40:08 -0600

    Only show divider if there are more than 1 chat

--- src/components/sidenav/ChatsList.tsx
+++ src/components/sidenav/ChatsList.tsx
@@ -57,7 +57,7 @@ export const ChatsList = () => {
                     onContextMenu={handleContextMenu}
                   />
                 ))}
-                <Divider />
+                {chatsSummary.length > 0 && <Divider />}
               </>
             )}
           </>
