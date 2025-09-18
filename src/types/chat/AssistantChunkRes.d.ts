75d9e2795ce7a57bd133ef895d3ea6b2194e0cec
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-17 20:08:30 -0600

    Add properties

--- src/types/chat/AssistantChunkRes.d.ts
+++ src/types/chat/AssistantChunkRes.d.ts
@@ -1,3 +1,13 @@
 type AssistantChunkRes = {
   content: string;
+  isLastChunk: boolean;
+
+  // Properties only present in the last chunk
+  chatId?: string; // UUID as string
+  chatTitle?: string;
+  promptTokens?: number;
+  completionTokens?: number;
+  totalTokens?: number;
+  totalChatPromptTokens?: number;
+  totalChatCompletionTokens?: number;
 };
