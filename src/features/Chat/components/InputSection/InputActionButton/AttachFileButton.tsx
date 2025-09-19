513c091d823b3aa53fb11a770f337ce458e26084
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-18 19:38:57 -0600

    Hide button while recording or sending audio

--- src/features/Chat/components/InputSection/InputActionButton/AttachFileButton.tsx
+++ src/features/Chat/components/InputSection/InputActionButton/AttachFileButton.tsx
@@ -1,13 +1,16 @@
 import { PaperClipIcon } from "icons/PaperClipIcon";
+import { useChatStore } from "store/app/ChatStore";
 
 type Props = {
   buttonClassName?: string;
 };
 
 export const AttachFileButton = ({ buttonClassName }: Props) => {
+  const { isRecordingAudio, isSendingAudio } = useChatStore();
+
   return (
     <button
-      className={`${buttonClassName}`}
+      className={`${buttonClassName} ${isRecordingAudio || isSendingAudio ? "hidden" : ""}`}
       type="button"
       aria-label="Attach file"
       title="Attach file"
