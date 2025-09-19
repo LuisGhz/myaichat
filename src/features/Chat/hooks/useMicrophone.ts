f02dce654c9b356ba38b288a3d96adca83450a2c
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-18 19:29:39 -0600

    Create useMicrophone

---
+++ src/features/Chat/hooks/useMicrophone.ts
@@ -0,0 +1,22 @@
+import { useChatStoreActions } from "store/app/ChatStore";
+import { transcribeAudioService } from "../services/ChatService";
+
+export const useMicrophone = () => {
+  const { setIsSendingAudio } = useChatStoreActions();
+
+  const transcribeAudio = async (audioBlob: Blob) => {
+    try {
+      setIsSendingAudio(true);
+      const result = await transcribeAudioService(audioBlob);
+      return result;
+    } catch (error) {
+      console.error("Error transcribing audio:", error);
+    } finally {
+      setIsSendingAudio(false);
+    }
+  };
+
+  return {
+    transcribeAudio,
+  };
+};
