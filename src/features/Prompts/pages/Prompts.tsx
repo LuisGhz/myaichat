e9f25f32a3c46f90c2b3d135ca0ee3c18adc3572
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-23 15:53:50 -0600

    Show prompts from service

--- src/features/Prompts/pages/Prompts.tsx
+++ src/features/Prompts/pages/Prompts.tsx
@@ -1,8 +1,18 @@
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { Button } from "antd";
+import { usePrompts } from "../hooks/usePrompts";
+import { useEffect } from "react";
+import { TrashOutlineIcon } from "icons/TrashOutlineIcon";
 
 export const Prompts = () => {
   const navigate = useNavigate();
+  const { getAllPrompts, promptsSummary } = usePrompts();
+
+  useEffect(() => {
+    getAllPrompts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <section className="flex justify-center items-center h-full flex-col gap-4">
       <h2 className="text-lg text-center app-text font-semibold">
@@ -10,18 +20,24 @@ export const Prompts = () => {
       </h2>
       <section>
         <ul className="app-text">
-          <li className="cursor-pointer w-48 py-1.5 hover:bg-gray-400 hover:text-shadow-gray-800 dark:hover:bg-gray-600 dark:hover:text-shadow-gray-200 transition-colors duration-200 rounded-xs px-0.5">
-            Prompt 1
-          </li>
-          <li className="cursor-pointer w-48 py-1.5 hover:bg-gray-400 hover:text-shadow-gray-800 dark:hover:bg-gray-600 dark:hover:text-shadow-gray-200 transition-colors duration-200 rounded-xs px-0.5">
-            Prompt 2
-          </li>
-          <li className="cursor-pointer w-48 py-1.5 hover:bg-gray-400 hover:text-shadow-gray-800 dark:hover:bg-gray-600 dark:hover:text-shadow-gray-200 transition-colors duration-200 rounded-xs px-0.5">
-            Prompt 3
-          </li>
-          <li className="cursor-pointer w-48 py-1.5 hover:bg-gray-400 hover:text-shadow-gray-800 dark:hover:bg-gray-600 dark:hover:text-shadow-gray-200 transition-colors duration-200 rounded-xs px-0.5">
-            Prompt 4
-          </li>
+          {promptsSummary.map((prompt) => (
+            <li
+              key={prompt.id}
+              className="cursor-pointer w-48 py-1.5 hover:bg-gray-400 hover:text-shadow-gray-800 dark:hover:bg-gray-600 dark:hover:text-shadow-gray-200 transition-colors duration-200 rounded-xs px-0.5 flex justify-between"
+            >
+              <Link className="!text-inherit" to={`/prompts/${prompt.id}`}>
+                {prompt.name}
+              </Link>
+              <button
+                className="hover:text-red-600 text-inherit transition-c-200 cursor-pointer text-lg"
+                aria-label="Delete Prompt"
+                title="Delete Prompt"
+                type="button"
+              >
+                <TrashOutlineIcon />
+              </button>
+            </li>
+          ))}
         </ul>
       </section>
       <section>
