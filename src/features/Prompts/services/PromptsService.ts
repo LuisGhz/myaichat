b75e47b03a8ac742fcab116d014dd19b1aeb7cbd
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-23 19:38:38 -0600

    Add delete prompt

--- src/features/Prompts/services/PromptsService.ts
+++ src/features/Prompts/services/PromptsService.ts
@@ -3,3 +3,7 @@ import { apiClient } from "api";
 export const GetAllPromptsService = async () => {
   return await apiClient.get<GetAllPromptsRes>("/custom-prompts/all");
 };
+
+export const deletePromptService = (promptId: string) => {
+  return apiClient.del(`/custom-prompts/${promptId}/delete`);
+};
