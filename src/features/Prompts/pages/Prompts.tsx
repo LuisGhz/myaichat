770da30364a6350cd4c852d3412abcc0c8d9d76a
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-23 19:32:27 -0600

    Add delete prompt functionality with confirmation modal

--- src/features/Prompts/pages/Prompts.tsx
+++ src/features/Prompts/pages/Prompts.tsx
@@ -1,17 +1,38 @@
 import { Link, useNavigate } from "react-router";
 import { Button } from "antd";
 import { usePrompts } from "../hooks/usePrompts";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { TrashOutlineIcon } from "icons/TrashOutlineIcon";
+import { ConfirmationModal } from "shared/modals/ConfirmationModal";
 
 export const Prompts = () => {
+  const [isDeleteConfirmationModalOpen, setIsDeleteConfirmationModalOpen] =
+    useState(false);
+  const [promptToDelete, setPromptToDelete] = useState<string | null>(null);
   const navigate = useNavigate();
-  const { getAllPrompts, promptsSummary } = usePrompts();
+  const { getAllPrompts, deletePrompt, promptsSummary } = usePrompts();
 
   useEffect(() => {
     getAllPrompts();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const askIfDelete = (promptId: string) => () => {
+    setIsDeleteConfirmationModalOpen(true);
+    setPromptToDelete(promptId);
+  };
+
+  const confirmDeletePrompt = async () => {
+    if (!promptToDelete) return;
+    await deletePrompt(promptToDelete);
+    setIsDeleteConfirmationModalOpen(false);
+    setPromptToDelete(null);
+  };
+
+  const cancelDeletePrompt = () => {
+    setIsDeleteConfirmationModalOpen(false);
+    setPromptToDelete(null);
+  };
 
   return (
     <section className="flex justify-center items-center h-full flex-col gap-4">
@@ -33,6 +54,7 @@ export const Prompts = () => {
                 aria-label="Delete Prompt"
                 title="Delete Prompt"
                 type="button"
+                onClick={askIfDelete(prompt.id)}
               >
                 <TrashOutlineIcon />
               </button>
@@ -50,6 +72,12 @@ export const Prompts = () => {
           Create New Prompt
         </Button>
       </section>
+      <ConfirmationModal
+        message={["Are you sure you want to delete this prompt?"]}
+        isOpen={isDeleteConfirmationModalOpen}
+        onClose={cancelDeletePrompt}
+        onConfirm={confirmDeletePrompt}
+      />
     </section>
   );
 };
