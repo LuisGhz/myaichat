f2506c20a4a5cad6cb10c315fdea32aac726ccb9
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-24 15:40:16 -0600

    Force unmount on chat with messages

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,11 +2,17 @@ import App from "App";
 import { ProtectedRoute } from "components/ProtectedRoute";
 import { Login } from "features/Auth/pages/Login";
 import { OAuth2Callback } from "features/Auth/pages/OAuth2Callback";
+import { useChatParams } from "features/Chat/hooks/useChatParams";
 import { Chat } from "features/Chat/pages/Chat";
 import { CreateEditPrompt } from "features/Prompts/pages/CreateEditPrompt";
 import { Prompts } from "features/Prompts/pages/Prompts";
 import { Welcome } from "features/Welcome/pages/Welcome";
 import { BrowserRouter, Route, Routes } from "react-router";
+
+const ChatWithMessages = () => {
+  const params = useChatParams();
+  return <Chat key={params.id} />;
+};
 
 export const AppRouter = () => {
   return (
@@ -24,7 +30,7 @@ export const AppRouter = () => {
         >
           <Route index element={<Welcome />} />
           <Route path="chat" element={<Chat />} />
-          <Route path="chat/:id" element={<Chat />} />
+          <Route path="chat/:id" element={<ChatWithMessages />} />
           <Route path="prompts" element={<Prompts />} />
           <Route path="prompts/form" element={<CreateEditPrompt />} />
           <Route path="prompts/form/:id" element={<CreateEditPrompt />} />
