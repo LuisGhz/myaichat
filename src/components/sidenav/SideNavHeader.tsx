65dbae858325900555b7635a87a72a284e527372
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-09-24 10:20:45 -0600

    Add logo

--- src/components/sidenav/SideNavHeader.tsx
+++ src/components/sidenav/SideNavHeader.tsx
@@ -1,5 +1,6 @@
 import { MenuFoldLeftIcon } from "icons/MenuFoldLeftIcon";
 import { useAppStore, useAppStoreActions } from "store/app/AppStore";
+import myaichatlogo from "assets/myaichat.png";
 
 export const SideNavHeader = () => {
   const { sideNavCollapsed } = useAppStore();
@@ -11,7 +12,11 @@ export const SideNavHeader = () => {
 
   return (
     <div className="flex justify-between items-center p-2.5">
-      <div className="w-8 h-8 xl:w-8 xl:h-8 bg-blue-600 rounded-full"></div>
+      <img
+        className="w-8 h-8 xl:w-8 xl:h-8 rounded-full"
+        src={myaichatlogo}
+        alt="MyAIChatlogo"
+      />
       <button onClick={handleToggle}>
         <MenuFoldLeftIcon
           className={`w-6 h-6 xl:w-8 xl:h-8 transition-transform dark:text-white cursor-pointer`}
