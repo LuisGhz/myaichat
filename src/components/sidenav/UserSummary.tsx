ab1875cadd507e06117bc75657d8354b4a1c27fb
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-16 10:42:41 -0600

    Change buttons styles to make it more accessible

--- src/components/sidenav/UserSummary.tsx
+++ src/components/sidenav/UserSummary.tsx
@@ -9,7 +9,11 @@ export const UserSummary = () => {
 
   return (
     <div className="flex gap-3 px-2.5">
-      <img className="w-12 rounded-full" src={user.avatarUrl} alt={user.email} />
+      <img
+        className="w-12 rounded-full"
+        src={user.avatarUrl}
+        alt={user.email}
+      />
       <div className="grow">
         <p
           className="app-text truncate whitespace-nowrap overflow-hidden mb-1"
@@ -19,7 +23,7 @@ export const UserSummary = () => {
         </p>
         <div className="text-end">
           <button
-            className="!bg-red-500 text-white py-0.5 px-2 rounded-sm text-xs cursor-pointer"
+            className="!bg-red-700 hover:!bg-red-800 text-white py-1 px-3 rounded-sm text-xs focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-400 cursor-pointer"
             type="button"
             onClick={logout}
             aria-label="Logout"
