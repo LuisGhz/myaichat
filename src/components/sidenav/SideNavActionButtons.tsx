85ff326ba88443a1a39d08cf84c32c198f0c8a0d
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-11-05 13:35:09 -0600

    Change button styles

--- src/components/sidenav/SideNavActionButtons.tsx
+++ src/components/sidenav/SideNavActionButtons.tsx
@@ -15,7 +15,7 @@ export const SideNavActionButtons = () => {
   return (
     <div className="flex flex-col gap-2 px-4">
       <button
-        className="bg-blue-500 hover:bg-blue-400 dark:bg-gray-950 dark:hover:bg-gray-950 dark:hover:outline-1 dark:hover:outline-gray-500 text-white py-1.5 rounded flex items-center justify-center gap-2 cursor-pointer"
+        className="dark:hover:outline-1 app-text font-bold py-1.5 rounded flex items-center justify-center gap-2 cursor-pointer"
         aria-label="New conversation"
         type="button"
         onClick={handleNavigation("/chat")}
@@ -24,7 +24,7 @@ export const SideNavActionButtons = () => {
         <span className="text-sm">New conversation</span>
       </button>
       <button
-        className="bg-blue-500 hover:bg-blue-400 dark:bg-gray-950 dark:hover:bg-gray-950 dark:hover:outline-1 dark:hover:outline-gray-500 text-white py-1.5 rounded flex items-center justify-center gap-2 cursor-pointer"
+        className="dark:hover:outline-1 app-text font-bold py-1.5 rounded flex items-center justify-center gap-2 cursor-pointer"
         aria-label="Prompts"
         type="button"
         onClick={handleNavigation("/prompts")}
