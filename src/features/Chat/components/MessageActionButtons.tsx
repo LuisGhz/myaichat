f1a16bcecb6f42fa7f2984b0d77cdd60ebe2edcb
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-11-05 14:42:31 -0600

    Minor styles changes

--- src/features/Chat/components/MessageActionButtons.tsx
+++ src/features/Chat/components/MessageActionButtons.tsx
@@ -35,13 +35,17 @@ export const MessageActionButtons = ({ role, content }: Props) => {
         <SoundMaxFillIcon />
       </button> */}
       <button
-        className="bg-gray-400 hover:bg-gray-500 dark:bg-gray-950 dark:hover:bg-gray-800 text-app transition-colors duration-200 p-1 rounded cursor-pointer"
+        className="bg-gray-300 hover:bg-woodsmoke-500 dark:bg-woodsmoke-950 dark:hover:bg-woodsmoke-800 app-text transition-colors duration-200 p-1 rounded cursor-pointer"
         type="button"
         title="Copy message to clipboard"
         aria-label="Copy message to clipboard"
         onClick={handleCopyToClipboard}
       >
-        {showCopied ? <BaselineCheckIcon /> : <BaselineContentCopyIcon />}
+        {showCopied ? (
+          <BaselineCheckIcon />
+        ) : (
+          <BaselineContentCopyIcon className="app-text" />
+        )}
       </button>
     </section>
   );
